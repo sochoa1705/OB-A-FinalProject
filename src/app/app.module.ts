a9086420208bafb50b458ca580a85f5a8cc72803
Sebastian Ochoa <38465363+sochoa1705@users.noreply.github.com>
2023-04-26 11:02:38 -0500

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TasklistComponent } from './components/tasklist/tasklist.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskFormComponent } from './components/task-form/task-form.component';
+import { LoginFormComponent } from './components/auth/login-form/login-form.component';
+import { RegisterFormComponent } from './components/auth/register-form/register-form.component';
+import { NavBarComponent } from './components/nav/nav-bar/nav-bar.component';
+import { HomePageComponent } from './components/pages/home-page/home-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { ErrorPageComponent } from './components/pages/error-page/error-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Import the ListsModule, personalized module
+import { ListsModule } from './modules/lists/lists.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TasklistComponent,
+    TaskComponent,
+    TaskFormComponent,
+    LoginFormComponent,
+    RegisterFormComponent,
+    NavBarComponent,
+    HomePageComponent,
+    LoginPageComponent,
+    RegisterPageComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ListsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
